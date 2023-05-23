8b67bb87fa5da80dc582a92e7a133f0adb1e3854
WilcoApp <hello@wilco.gg>
2023-05-23 12:08:23 +0000

    Fixing A Bug

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,21 +3,14 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import React from "react";
 import { store } from "./store";
-import {ThemeProvider} from 'styled-components'
 
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
 
-import theme from "./styles/theme";
-import GlobalStyle from "./styles/GlobalStyle";
-
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter basename="/">
-      <ThemeProvider theme={theme}>
-        <GlobalStyle/>
-        <App />
-      </ThemeProvider>
+      <App />
     </BrowserRouter>
   </Provider>,
 
