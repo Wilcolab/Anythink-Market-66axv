bf297d4ad0271c788d55c3c4939d07ae647065ad
MenachemZeivald <108309381+MenachemZeivald@users.noreply.github.com>
2023-05-23 13:22:25 +0300

    add theme to banner

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,18 +1,27 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
+import styled, { ThemeProvider } from "styled-components";
+import theme from "../../styles/theme";
 
+const ThemeDiv = styled.div`
+background-color: ${p => p.theme.dark};
+`
 const Banner = () => {
   return (
-    <div className="banner text-white">
-      <div className="container p-4 text-center">
-        <img src={logo} alt="banner" />
-        <div>
-          <span>A place to </span>
-          <span id="get-part">get</span>
-          <span> the cool stuff.</span>
+    <ThemeProvider theme={theme}>
+      <ThemeDiv>
+        <div className="banner text-white">
+          <div className="container p-4 text-center">
+            <img src={logo} alt="banner" />
+            <div>
+              <span>A place to </span>
+              <span id="get-part">get</span>
+              <span> the cool stuff.</span>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </ThemeDiv>
+    </ThemeProvider>
   );
 };
 
