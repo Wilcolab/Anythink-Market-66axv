286ddb9cd1676f1d3cde94fae550bdd9e89506a3
MenachemZeivald <108309381+MenachemZeivald@users.noreply.github.com>
2023-05-23 13:10:35 +0300

    Add theme provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,19 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import React from "react";
 import { store } from "./store";
+import {ThemeProvider} from 'styled-components'
 
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
 
+import theme from "./styles/theme";
+
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter basename="/">
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </Provider>,
 
